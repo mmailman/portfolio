55c9e4e6aff3bccc9638ac12cfd1c9b46663deeb
Michael Mailman <mmailman@gmail.com>
2016-05-10 13:56:35 -0700

    Reworking html to handle a project object, which is defined in the app.js that was added.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var projects = [];
+
+function Project(project){
+  this.projectName = project.projectName;
+  this.imgPath = project.imgPath;
+  this.lastUpdated = project.lastUpdated;
+  this.description = project.description;
+  
+}
